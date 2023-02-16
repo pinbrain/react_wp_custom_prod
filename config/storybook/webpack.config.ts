1b1a872f7d3069aae0134d4545d35a3d122e1b96
Pavel Kuzikov <pinbrain@yandex.ru>
2023-02-16 21:56:03 +0300

    fix lint in wp conf for sb

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -14,7 +14,8 @@ export default ({ config }: { config: webpack.Configuration }) => {
   config.resolve?.modules?.push(paths.src);
   config.resolve?.extensions?.push('.ts', '.tsx');
 
-  config!.module!.rules = config!.module!.rules!.map((rule: any) => {
+  // eslint-disable-next-line no-param-reassign
+  config.module!.rules! = config!.module!.rules!.map((rule: any) => {
     if (/svg/.test(rule.test as string)) {
       return { ...rule, exclude: /\.svg$/i };
     }
