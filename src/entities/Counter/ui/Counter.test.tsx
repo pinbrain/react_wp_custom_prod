62161cc1bcf25a804180769d6ceedb7de30ec28c
Pavel Kuzikov <pinbrain@yandex.ru>
2023-02-21 19:43:17 +0300

    add redux store; first test entity

---
+++ src/entities/Counter/ui/Counter.test.tsx
@@ -0,0 +1,22 @@
+import { fireEvent, screen } from '@testing-library/react';
+import { ComponentRender } from 'shared/lib/tests/ComponentRender/ComponentRender';
+import { Counter } from '..';
+
+describe('Counter', () => {
+  test('Render', () => {
+    ComponentRender(<Counter />, { initialState: { counter: { value: 10 } } });
+    expect(screen.getByTestId('value-title')).toHaveTextContent('10');
+  });
+
+  test('increment', () => {
+    ComponentRender(<Counter />, { initialState: { counter: { value: 10 } } });
+    fireEvent.click(screen.getByTestId('increment-btn'));
+    expect(screen.getByTestId('value-title')).toHaveTextContent('11');
+  });
+
+  test('decrement', () => {
+    ComponentRender(<Counter />, { initialState: { counter: { value: 10 } } });
+    fireEvent.click(screen.getByTestId('decrement-btn'));
+    expect(screen.getByTestId('value-title')).toHaveTextContent('9');
+  });
+});
