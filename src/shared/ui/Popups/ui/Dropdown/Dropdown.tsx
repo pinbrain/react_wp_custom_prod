3044c46b02d82ceef0102f4def46bfcdb22bc2a3
Pavel Kuzikov <pinbrain@yandex.ru>
2023-05-04 20:59:18 +0300

    add star rating; add rating entity and rating card

--- src/shared/ui/Popups/ui/Dropdown/Dropdown.tsx
+++ src/shared/ui/Popups/ui/Dropdown/Dropdown.tsx
@@ -44,14 +44,20 @@ export const Dropdown = memo((props: DropdownProps) => {
 
           if (item.href) {
             return (
-              <Menu.Item as={AppLink} refName="href" to={item.href} disabled={item.disabled} key={index}>
+              <Menu.Item
+                as={AppLink}
+                refName="href"
+                to={item.href}
+                disabled={item.disabled}
+                key={`dropdown-key-${index}`}
+              >
                 {content}
               </Menu.Item>
             );
           }
 
           return (
-            <Menu.Item as={Fragment} disabled={item.disabled} key={index}>
+            <Menu.Item as={Fragment} disabled={item.disabled} key={`dropdown-key-${index}`}>
               {content}
             </Menu.Item>
           );
