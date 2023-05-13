01b83273e2d29950f1cd8ae53d6b78f582710e16
Pavel Kuzikov <pinbrain@yandex.ru>
2023-05-13 17:22:45 +0300

    add theme addon for storybook

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -13,6 +13,14 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  themes: {
+    default: 'light',
+    list: [
+      { name: 'light', class: Theme.LIGHT, color: '#ffffff' },
+      { name: 'dark', class: Theme.DARK, color: '#000000' },
+      { name: 'orange', class: Theme.ORANGE, color: '#ffb005' },
+    ],
+  },
 };
 
 addDecorator(StyleDecorator);
