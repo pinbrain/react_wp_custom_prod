8906c4322d084726b697d028b0f5268ec309844b
Pavel Kuzikov <pinbrain@yandex.ru>
2023-05-13 21:43:36 +0300

    remove test counter

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Page } from '@/widgets/Page';
-import { Counter } from '@/entities/Counter';
 
 const MainPage = () => {
   const { t } = useTranslation('main');
 
-  return (
-    <Page>
-      <Counter />
-      {t('Главная страница', { ns: 'main' })}
-    </Page>
-  );
+  return <Page>{t('Главная страница', { ns: 'main' })}</Page>;
 };
 
 export default MainPage;
