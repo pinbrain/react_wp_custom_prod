3528076ae0150d0c2dbf9aa96ff21b58ed5fb261
Pavel Kuzikov <pinbrain@yandex.ru>
2023-05-13 21:42:45 +0300

    add helpers for slice, selectors, actions

--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -1,29 +1,39 @@
 import { useTranslation } from 'react-i18next';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { Button } from '@/shared/ui/Button';
-import { getCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
-import { counterActions } from '../model/slice/counterSlice';
+import { useCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
+import { useCounterActions } from '../model/slice/counterSlice';
 
 export const Counter = () => {
   const dispatch = useDispatch();
-  const counterValue = useSelector(getCounterValue);
+  const counterValue = useCounterValue();
   const { t } = useTranslation();
+  const { increment, decrement, add } = useCounterActions();
 
-  const increment = () => {
-    dispatch(counterActions.increment());
+  const handleInc = () => {
+    // dispatch(counterActions.increment());
+    increment();
   };
 
-  const decrement = () => {
-    dispatch(counterActions.decrement());
+  const handleDec = () => {
+    // dispatch(counterActions.decrement());
+    decrement();
+  };
+
+  const handleAddFive = () => {
+    add(5);
   };
 
   return (
     <div>
       <h1 data-testid="value-title">{counterValue}</h1>
-      <Button data-testid="increment-btn" onClick={increment}>
+      <Button data-testid="increment-btn" onClick={handleInc}>
         {t('increment')}
       </Button>
-      <Button data-testid="decrement-btn" onClick={decrement}>
+      <Button data-testid="increment-btn" onClick={handleAddFive}>
+        +5
+      </Button>
+      <Button data-testid="decrement-btn" onClick={handleDec}>
         {t('decrement')}
       </Button>
     </div>
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Page } from '@/widgets/Page';
+import { Counter } from '@/entities/Counter';
 
 const MainPage = () => {
   const { t } = useTranslation('main');
 
-  return <Page>{t('Главная страница', { ns: 'main' })}</Page>;
+  return (
+    <Page>
+      <Counter />
+      {t('Главная страница', { ns: 'main' })}
+    </Page>
+  );
 };
 
 export default MainPage;
