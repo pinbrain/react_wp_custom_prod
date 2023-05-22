ab88ebee3d1d03ade98080eb38aff4257905a1b9
Pavel Kuzikov <pinbrain@yandex.ru>
2023-05-22 19:16:51 +0300

    add tests for isolated components with cypress

--- src/shared/lib/tests/ComponentRender/ComponentRender.tsx
+++ src/shared/lib/tests/ComponentRender/ComponentRender.tsx
@@ -5,20 +5,41 @@ import { I18nextProvider } from 'react-i18next';
 import { MemoryRouter } from 'react-router-dom';
 import { StateSchema, StoreProvider } from '@/app/providers/StoreProvider';
 import i18nForTests from '@/shared/config/i18n/i18nForTests';
+import { Theme } from '@/shared/const/theme';
+// eslint-disable-next-line pinbrain-path-check-plugin/layer-imports
+import { ThemeProvider } from '@/app/providers/ThemeProvider';
+// eslint-disable-next-line pinbrain-path-check-plugin/layer-imports
+import '@/app/styles/index.scss';
 
 export interface ComponentRenderOptions {
   route?: string;
   initialState?: DeepPartial<StateSchema>;
   asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>;
+  theme?: Theme;
 }
 
-export function ComponentRender(component: ReactNode, options: ComponentRenderOptions = {}) {
-  const { route = '/', initialState, asyncReducers } = options;
-  return render(
+interface TestProviderProps {
+  children: ReactNode;
+  options?: ComponentRenderOptions;
+}
+
+export function TestProvider(props: TestProviderProps) {
+  const { children, options = {} } = props;
+  const { route = '/', initialState, asyncReducers, theme = Theme.LIGHT } = options;
+
+  return (
     <MemoryRouter initialEntries={[route]}>
       <StoreProvider initialState={initialState} asyncReducers={asyncReducers}>
-        <I18nextProvider i18n={i18nForTests}>{component}</I18nextProvider>
+        <I18nextProvider i18n={i18nForTests}>
+          <ThemeProvider initialTheme={theme}>
+            <div className={`app ${theme}`}>{children}</div>
+          </ThemeProvider>
+        </I18nextProvider>
       </StoreProvider>
     </MemoryRouter>
   );
+}
+
+export function ComponentRender(component: ReactNode, options: ComponentRenderOptions = {}) {
+  return render(<TestProvider options={options}>{component}</TestProvider>);
 }
