964bee363e5aa53da590fd2627b3f98302e305cb
Pavel Kuzikov <pinbrain@yandex.ru>
2023-06-17 12:48:35 +0300

    add https for json server

--- json-server/index.js
+++ json-server/index.js
@@ -1,6 +1,12 @@
 const fs = require('fs');
 const jsonServer = require('json-server');
 const path = require('path');
+const https = require('https');
+
+const options = {
+  key: fs.readFileSync(path.resolve(__dirname, 'key.pem')),
+  cert: fs.readFileSync(path.resolve(__dirname, 'cert.pem')),
+};
 
 const server = jsonServer.create();
 
@@ -21,10 +27,14 @@ server.use(async (req, res, next) => {
 server.post('/login', (req, res) => {
   try {
     const { username, password } = req.body;
-    const db = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'));
+    const db = JSON.parse(
+      fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'),
+    );
     const { users = [] } = db;
 
-    const userFromBd = users.find((user) => user.username === username && user.password === password);
+    const userFromBd = users.find(
+      (user) => user.username === username && user.password === password,
+    );
 
     if (userFromBd) {
       return res.json(userFromBd);
@@ -49,7 +59,13 @@ server.use((req, res, next) => {
 
 server.use(router);
 
+const httpsServer = https.createServer(options, server);
+
 // запуск сервера
 server.listen(8000, () => {
   console.log('server is running on 8000 port');
+});
+
+httpsServer.listen(443, () => {
+  console.log('server is running on 443 port');
 });
